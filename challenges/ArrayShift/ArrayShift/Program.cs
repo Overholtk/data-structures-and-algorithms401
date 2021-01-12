895ace539ab1dc955c8529adde44720389d4b7d6
overholtk <overholtkaya@gmail.com>
2021-01-12 15:25:04 -0800

    finished challenge 2

---
+++ challenges/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    class ArrayShift
+    {
+        static void Main(string[] args)
+        {
+            InsertShiftArray(new int[4] { 0, 1, 2, 3 }, 4); 
+        }
+
+        static int[] InsertShiftArray(int[] arr, int num)
+        {
+            for(var i = 0; i < arr.Length; i++)
+            {
+            Console.WriteLine(arr[i]);
+            }
+            int mid = 0;
+            int length = arr.Length;
+            int l = length + 1;
+            if(length % 2 == 0)
+            {
+                mid = length / 2;
+            }
+            else
+            {
+                mid = (length / 2) + 1;
+            }
+            int[] newArr = new int[l];
+            newArr[mid] = num;
+            for(int i = 0; i < l; i++)
+            {
+                if(i < mid)
+                {
+                    newArr[i] = arr[i];
+                }
+                if(i > mid)
+                {
+                    newArr[i] = arr[i - 1];
+                }
+            }
+            for(int i = 0; i < newArr.Length; i++)
+            {
+            Console.WriteLine(newArr[i]);
+            }
+            return newArr;
+        }
+    }
+}
---
+++ challenges/ArrayShift/README.md
@@ -0,0 +1,9 @@
+# Code Challenge 2: Array Shift
+
+## Summary:
+Write a method that takes in an array and a value as parameters, then returns an array with the value inserted in the middle.
+
+## Approach & Efficiency
+I chose to use a for loop to insert data into the new array. To cut down on lines of code I added the param value into the array outside of the for loop.
+
+## Solution
