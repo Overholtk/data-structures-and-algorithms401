510c86fb96ee91df62928b264f1f912167d46896
overholtk <overholtkaya@gmail.com>
2021-01-15 21:30:37 -0800

    finished challenge

---
+++ challenges/LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LinkedLists
+{
+        public class LinkedList
+        {
+
+            public Node Head { get; set; }
+
+            public LinkedList() { }
+
+            public void Insert(int value)
+            {
+                Node node = new Node(value);
+                node.Next = Head;
+                Head = node;
+            }
+
+            public bool Includes(int value)
+            {
+                Node current = Head;
+                bool valueFound = false;
+                while (current != null)
+                {
+                    if(current.Value == value)
+                    {
+                        valueFound = true;
+                    }
+                    current = current.Next;
+                }
+                return valueFound;
+            }
+
+            public string toString()
+            {
+                Node current = Head;
+                string allValues = "";
+                while(current != null)
+                {
+                    allValues += ($"{current.Value} => ");
+                    current = current.Next;
+                }
+                allValues += ("NULL");
+            return allValues;
+            }
+        }
+}
---
+++ challenges/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList myList = new LinkedList();
+
+            myList.Insert(1);
+            myList.Insert(2);
+            myList.Insert(3);
+            myList.Insert(4);
+            Console.WriteLine(myList.toString());
+            Console.Write(myList.Includes(50));
+
+        }
+
+    }
+}
