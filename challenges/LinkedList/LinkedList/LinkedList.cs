a80a6c835be1b3c8baf6dab07c3ac2ea58d482e0
moregaurd@outlook.com <moregaurd@outlook.com>
2021-01-18 21:59:34 -0800

    test to see if this is why none of my pushes are working

--- challenges/LinkedList/LinkedList/LinkedList.cs
+++ challenges/LinkedList/LinkedList/LinkedList.cs
@@ -2,21 +2,21 @@
 
 namespace LinkedLists
 {
-        public class LinkedList
+    public class LinkedList
         {
 
-            public Node Head { get; set; }
+        public Node Head { get; set; }
 
-            public LinkedList() { }
+        public LinkedList() { }
 
-            public void Insert(int value)
+        public void Insert(int value)
             {
                 Node node = new Node(value);
                 node.Next = Head;
                 Head = node;
             }
 
-            public bool Includes(int value)
+        public bool Includes(int value)
             {
                 Node current = Head;
                 bool valueFound = false;
@@ -31,7 +31,7 @@ namespace LinkedLists
                 return valueFound;
             }
 
-            public string toString()
+        public string toString()
             {
                 Node current = Head;
                 string allValues = "";
@@ -43,5 +43,53 @@ namespace LinkedLists
                 allValues += ("NULL");
             return allValues;
             }
+
+        public int append(int value)
+        {
+            Node currentNode = Head;
+            while(currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+            Node newNode = new Node(value);
+            currentNode.Next = newNode;
+            return newNode.Value;
         }
+
+        public int insertBefore(int value, int newVal)
+        {
+            Node currentNode = Head;
+            Node previousNode = Head;
+            while(currentNode != null)
+            {
+                if(currentNode.Value == newVal)
+                {
+                    Node newNode = new Node(newVal);
+                    previousNode.Next = newNode;
+                    newNode.Next = currentNode;
+                    break;
+                }
+                previousNode = currentNode;
+                currentNode = currentNode.Next;
+            }
+            return currentNode.Value;
+        }
+
+        public int insertAfter(int value, int newVal)
+        {
+            Node current = Head;
+            Node newNode = new Node(value);
+            while(current != null)
+            {
+                if(current.Value == value)
+                {
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                    break;
+                }
+                current = current.Next;
+            }
+            return newNode.Next.Value;
+        }
+    }
 }
