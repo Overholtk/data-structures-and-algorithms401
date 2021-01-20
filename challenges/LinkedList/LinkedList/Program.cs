90c61ff5eb2dcc26668d1322e03a3decdc4df04c
moregaurd@outlook.com <moregaurd@outlook.com>
2021-01-19 21:09:38 -0800

    method complete - no tests

--- challenges/LinkedList/LinkedList/Program.cs
+++ challenges/LinkedList/LinkedList/Program.cs
@@ -13,6 +13,7 @@ namespace LinkedLists
             myList.Insert(2);
             myList.Insert(3);
             myList.Insert(4);
+            Console.WriteLine(myList.kthFromEnd(5));
             Console.WriteLine(myList.toString());
             Console.Write(myList.Includes(50));
 
