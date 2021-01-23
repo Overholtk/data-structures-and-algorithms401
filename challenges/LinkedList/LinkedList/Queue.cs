561c4e2c1ed44860bf5411270b61c9d51d40ea26
moregaurd@outlook.com <moregaurd@outlook.com>
2021-01-22 22:45:29 -0800

    few weird bugs not sure what is wrong with tests- need to grab a TA

--- challenges/LinkedList/LinkedList/Queue.cs
+++ challenges/LinkedList/LinkedList/Queue.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace LinkedLists
 {
-    class Queue
+    class Queue<T>
     {
+        public Node<T> front { get; set; }
+
+        public void enqueue(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            node.Next = front;
+            front = node;
+        }
+
+        public Node<T> dequeue()
+        {
+            Node<T> currentFront = front;
+            front = currentFront.Next;
+            currentFront.Next = null;
+            return currentFront;
+        }
+
+        public T peek()
+        {
+            return front.Value;
+        }
+
+        public bool isEmpty()
+        {
+            return front != null;
+        }
+
     }
 }
---
+++ challenges/LinkedList/LinkedListTests/StackAndQueueTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using System.Text;
+using LinkedLists;
+
+namespace LinkedListTests
+{
+    public class StackAndQueueTests
+    {
+        [Fact]
+        public void Push_To_Stack()
+        {
+            Stack<string> test = new Stack<string>();
+            test.Push("15");
+            Assert.Equal("15", test.top);
+
+        }
+    }
+}
